06da74c6fed656f0c9ffe24037df7e0fe3d14bb3
Jacob Teo <jacobtpl@gmail.com>
2018-10-13 22:13:09 +0800

    add stuff

--- facedetect.py
+++ facedetect.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 face_cascade = cv.CascadeClassifier(cv.data.haarcascades+'haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier(cv.data.haarcascades+'haarcascade_eye.xml')
-img = cv.imread('img2.jpg')
+img = cv.imread('img.jpg')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
