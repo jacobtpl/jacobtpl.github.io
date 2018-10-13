9339b12b2c0e8abe513791baf58acbfae8e94bde
Dylan <mathgeekdyl@gmail.com>
2018-10-14 06:50:56 +0800

    attempt

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ from flask import Flask, render_template, request
 from werkzeug import secure_filename
 app = Flask(__name__)
 
-@app.route('/upload')
-def upload():
-   return render_template('trialPost.html')
+@app.route('/')
+def home():
+   return render_template('trialPost.html')#index.html
 	
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/picDisplay', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
+      
+      #perform desired function on f: facialCommunism     
       f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
-		
-app.run()
+      return render_template('picDisplay.html')
+
+@app.route('/picTaking', methods = ['GET', 'POST'])
+def undo():
+   if request.method == 'POST':
+      f = request.files['file']
+
+      #perform desired function on f      
+      #f.save(secure_filename(f.filename))
+      return render_template('vidya.html')
+
+
